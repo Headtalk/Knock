5dd769f7c518b20078a7e0d64edbbdfeee476e71
Alexander List <atlex2@gmail.com>
2015-09-20 12:56:43 -0700

    cleaned out unecessary code

--- Knock/ViewController.m
+++ Knock/ViewController.m
@@ -8,13 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
-
-
 -(void)knockDetectorDetectedKnock:(HTKnockDetector*)detector atTime:(NSTimeInterval)time{
     self.knockLabel.alpha = 1;
     [UIView animateWithDuration:.6 delay:.1 options:UIViewAnimationOptionAllowUserInteraction|UIViewAnimationOptionCurveEaseOut animations:^{
@@ -26,17 +20,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
     self.knockDetector = [[HTKnockDetector alloc] init];
     [self.knockDetector setDelegate:self];
     [self.knockDetector setIsOn:true];
     
     self.knockLabel.alpha = 0;
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
